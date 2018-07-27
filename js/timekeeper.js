c5091222407f465213f56ae3b42d8f852078d6a6
TakamiChie@Visnu <chie@onpu-tamago.net>
2018-07-27 19:08:05 +0900

    Update copyright.

--- js/timekeeper.js
+++ js/timekeeper.js
@@ -2,6 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2014-2016 Ichiro Maruta
+Copyright (c) 2018 TakamiChie
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
